d34cd9691f75f446bef5a4b4d2137364f6b48cc2
joshua7ko <89149045+joshua7ko@users.noreply.github.com>
2025-03-28 19:48:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    proxy: {
+      "/api": {
+        target: "https://auth-user-contact-project-production.up.railway.app",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       css: {
