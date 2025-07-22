cb1b947c04d29fcb40171289ea8ff46b78fcd7c9
joshua7ko <89149045+joshua7ko@users.noreply.github.com>
2025-07-22 18:24:12 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,16 +35,16 @@ const loginUser =async(req, res) => {
         const {username, password} = req.body;
         const user = await User.findOne({username})
         if(!user){
-            return res.status(401).json({error : 'Invalid Credentials'})
+            return res.status(401).json({message : 'Invalid Credentials'})
         }
         const isPasswordValid =await bcrypt.compare(password, user.password)
         if(!isPasswordValid){
-            return res.status(401).json({error: 'Invalid Credentials'})
+            return res.status(401).json({message: 'Invalid Credentials'})
         }
         const token = jwt.sign({ _id: user._id}, process.env.SECRET_KEY, {expiresIn: '1h'})
         res.json({message: 'User Login Successful', token: token})
     } catch (error) {
-        res.status(500).json({error: 'Error Logging in'})
+        res.status(500).json({message: 'Error Logging in'})
     }
 }
 
